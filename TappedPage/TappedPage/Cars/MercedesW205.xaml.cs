b0eafcbd5d2a437a973a465618a1ff7aa2abc485
Opilane TTHK <opilane@tthk.ee>
2020-10-14 10:09:34 +0300

    Add project files.

---
+++ TappedPage/TappedPage/Cars/MercedesW205.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TappedPage.Cars
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MercedesW205 : ContentPage
+    {
+        public MercedesW205()
+        {
+            InitializeComponent();
+        }
+    }
+}
